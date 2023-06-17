a7316bb54d06b4207514e3e9176732f115df3260
Beaver <8586904+PragmaticBeaver@users.noreply.github.com>
2023-06-17 12:59:30 +0200

    chore: fix ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,39 @@
 module.exports = {
-  root: true,
-  parser: "eslint/parser",
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: ["eslint:recommended", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+  globals: {
+    // FoundryVTT props
+    ui: "readonly",
+    game: "readonly",
+
+    // FoundryVTT classes
+    Hooks: "readonly",
+    Application: "readonly",
+
+    // FoundryVTT Namespace utils
+    mergeObject: "readonly",
+
+    // FoundryVTT client functions
+    loadTemplates: "readonly",
+    renderTemplate: "readonly",
+  },
 };
