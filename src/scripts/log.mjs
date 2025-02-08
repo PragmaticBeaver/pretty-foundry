b85bd2d3895a2f079fddd00d75d889b90c0f1a23
Beaver <8586904+PragmaticBeaver@users.noreply.github.com>
2025-02-08 22:23:09 +0100

    chore: colored logs

--- src/scripts/log.mjs
+++ src/scripts/log.mjs
@@ -1,61 +1,33 @@
-// export function logToConsole(...args) {
-//   console.log("Pretty Mixer |", ...args);
-// }
-
-// export function infoToConsole(...args) {
-//   console.info("Pretty Mixer |", ...args);
-// }
-
-// export function warnToConsole(...args) {
-//   console.warn("Pretty Mixer |", ...args);
-// }
-
-// export function errorToConsole(...args) {
-//   console.error("Pretty Mixer |", ...args);
-// }
-
 const PREFIX = "Pretty Mixer | ";
-const LOG_LEVEL = LOG_LEVEL_INDEX.DEBUG;
-
 const LOG_LEVEL_INDEX = {
   DEBUG: 0,
   INFO: 1,
   WARN: 2,
   ERROR: 3,
 };
+const LOG_LEVEL = LOG_LEVEL_INDEX.DEBUG;
 
-function _log(level, msg, data) {
-  const dataVal = data ? JSON.stringify(data) : "";
-  if (LOG_LEVEL >= LOG_LEVEL_INDEX[level]) {
-    console.log(PREFIX, msg, dataVal);
+export function logToConsole(msg, data) {
+  if (LOG_LEVEL > LOG_LEVEL_INDEX.DEBUG) {
+    return;
   }
+  console.log(`%c ${PREFIX} ${msg} ${data || ""}`, "color:#6495ED");
 }
 
-export const Logger = {
-  log: (msg, data) => _log(LOG_LEVEL_INDEX.DEBUG, msg, data),
-  info: () => {},
-  warn: () => {},
-  error: () => {},
-};
+export function infoToConsole(msg, data) {
+  if (LOG_LEVEL > LOG_LEVEL_INDEX.INFO) {
+    return;
+  }
+  console.info(`%c ${PREFIX} ${msg} ${data || ""}`, "color:green");
+}
 
-// static func debug(message: String, data: Variant = null):
-// 	if not DEBUG_CONFIG.log_debug:
-// 		return
-// 	_log("DEBUG", "white", message, data)
+export function warnToConsole(msg, data) {
+  if (LOG_LEVEL > LOG_LEVEL_INDEX.WARN) {
+    return;
+  }
+  console.warn(`%c ${PREFIX} ${msg} ${data || ""}`, "color:#DAA520");
+}
 
-// static func info(message: String, data: Variant = null):
-// 	if not DEBUG_CONFIG.log_info:
-// 		return
-// 	_log("INFO", "green", message, data)
-
-// static func warn(message: String, data: Variant = null):
-// 	if not DEBUG_CONFIG.log_warn:
-// 		return
-// 	push_warning(message, data)
-// 	_log("WARN", "yellow", message, data)
-
-// static func error(message: String, data: Variant = null):
-// 	if not DEBUG_CONFIG.log_error:
-// 		return
-// 	push_error(message, data)
-// 	_log("ERROR", "red", message, data)
+export function errorToConsole(msg, data) {
+  console.error(`%c ${PREFIX} ${msg} ${data || ""}`, "color:red");
+}
